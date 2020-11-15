f12ccbe36168d329762f3d8b04363349fb861499
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-14 22:16:20 -0300

    Instalacao e implementacao inicial da API VENOM BOT

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { Base } from './model';
 import { connectOn, connectOFF } from './dao/mysql';
+import { create, Whatsapp } from 'venom-bot';
 
-(async () => {
-  await connectOn({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'whatsapp',
+function main(whots: Whatsapp) {
+  whots.onMessage((message) => {
+    whots.sendText(
+      message.from,
+      'Bom dia! / Boa tarde! / Boa noite! ' +
+        'Sou uma atendente virtual. Com a sua autorização irei armazenar suas conversas.' +
+        ' Digite "SIM" para autorizar ou "NÃO" para proceguir sem o armazenamento.'
+    );
   });
+}
 
-  //
+try {
+  (async () => {
+    await connectOn({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'whatsapp',
+    });
 
-  await connectOFF();
-})();
+    let venom = await create();
+
+    main(venom);
+  })();
+} catch (error) {
+  connectOFF();
+}
